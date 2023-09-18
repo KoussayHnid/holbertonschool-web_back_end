75947fc239e6f1964cc8166ddbcad7871170a4e8
koussay557 <henidkousay@gmail.com>
2023-09-18 13:40:16 +0000

    task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
+"""This is a module for adding two numbers."""
+
 def add(a: float, b: float) -> float:
+     """
+    Add two numbers and return the result.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+        """
     return a + b
