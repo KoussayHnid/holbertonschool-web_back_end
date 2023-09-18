3a4f559f97960112fe422af7e79ab31e319528ad
koussay557 <henidkousay@gmail.com>
2023-09-18 13:59:00 +0000

    task 1

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 def concat(str1: str, str2: str) -> str:
+
+    """
+    Concatenate two strings and return the result as a string.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+        """
     return str1 + str2
