19150a41bcac2b20cc5c412a26d1762c69ad42d3
koussay557 <henidkousay@gmail.com>
2023-09-18 13:42:39 +0000

    task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,11 +2,14 @@
 """This is a module for adding two numbers."""
 
 def add(a: float, b: float) -> float:
-     """
+    """
     Add two numbers and return the result.
 
     Args:
         a (float): The first number.
         b (float): The second number.
-        """
+
+    Returns:
+        float: The sum of 'a' and 'b'.
+    """
     return a + b
