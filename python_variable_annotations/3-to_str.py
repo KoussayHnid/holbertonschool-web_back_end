7cbd957c43631611f365d90c9cc07555e467eabe
koussay557 <henidkousay@gmail.com>
2023-09-18 15:38:45 +0000

    task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def to_str(n: float) -> str:
+    """
+    Convert a float to its string representation
+
+    Args:
+        n (float): The float to be converted to a string
+        """
+    return str(n)
