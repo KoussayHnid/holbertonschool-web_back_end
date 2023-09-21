5ffe16dc45c17752df2e91f7edb3ad446f1c95d1
koussay557 <henidkousay@gmail.com>
2023-09-21 11:12:15 +0100

    task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,6 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import asyncio
 import random
+
+"function that will loop the coroutine 10 times "
 
 async def async_generator():
     for _ in range(10):
