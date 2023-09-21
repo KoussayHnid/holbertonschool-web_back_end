1288ab7cae2d1f2f697c411041401ab2fdb80628
koussay557 <henidkousay@gmail.com>
2023-09-21 11:14:13 +0100

    task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,8 +2,9 @@
 
 import asyncio
 import random
-"function that will loop the coroutine 10 times "
+
 async def async_generator():
+    "function that will loop the coroutine 10 times "
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.randint(0, 10)
