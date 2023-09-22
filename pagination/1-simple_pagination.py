382d09e670214ae76bea81060d6b7c8b3a3252ff
koussay557 <henidkousay@gmail.com>
2023-09-22 21:46:44 +0100

    task 1

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import csv
+import math
+from typing import List
+
+def index_range(page, page_size):
+    """
+    Return a tuple of start and end indexes for a given page and page size
+    """
+    if page <= 0 or page_size <= 0:
+        raise ValueError("Page and page_size must be positive integers.")
+
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return start_index, end_index
+
+class Server:
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        if not isinstance(
+                page,
+                int) or not isinstance(
+                page_size,
+                int) or page <= 0 or page_size <= 0:
+            raise AssertionError(
+                "AssertionError raised with negative values or non-integers")
+
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+
+        if start_index >= len(dataset):
+            return []
+
+        return dataset[start_index:end_index]
