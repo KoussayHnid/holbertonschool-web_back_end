827cffd0abd0c29b191f5edb99af9d82626a7fc7
koussay557 <henidkousay@gmail.com>
2023-09-22 19:01:26 +0100

    task 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+"""
+This module defines a function for calculating index ranges for pagination.
+"""
+
+
 def index_range(page, page_size):
     """
     Return a tuple of start and end indexes for a given page and page size
-    """
-    """Returns:
-        tuple: A tuple containing the start and end indexes (0-indexed) for the given page.
     """
     if page <= 0 or page_size <= 0:
         raise ValueError("Page and page_size must be positive integers.")
