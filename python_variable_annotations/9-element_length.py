7dd962c3ac0582e10d582b8080523ccadeef7b3d
koussay557 <henidkousay@gmail.com>
2023-09-25 18:49:23 +0100

    function takes a list of strings as input and returns a new list of tuples

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,6 +2,7 @@
 """function that return values with appropriate types"""
 from typing import List, Tuple
 
+
 def element_length(lst: List[str]) -> List[Tuple[str, int]]:
     """
     Calculate the length of elements in a list and return a list of tuples.
