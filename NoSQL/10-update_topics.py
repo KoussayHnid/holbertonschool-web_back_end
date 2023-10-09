36302ad5b8a073b0f3d012c2b6fcd075f10c6b7d
koussay557 <henidkousay@gmail.com>
2023-10-09 12:01:32 +0100

    script that make u to insert smthing in database

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -6,5 +6,4 @@ def updae_topics(mongo_collection, name, topics)
 
 
 """ that how to change all topics"""
-return mongo_collection.update_many(
-    {"name": name}, {"$set": {"topics": topics}})
+mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
