d4fdc143bcf4cfae91500b606a8b98ee60c65ce2
koussay557 <henidkousay@gmail.com>
2023-10-09 13:20:28 +0100

    function that will loop 10 times, each time asynchronously wait 1 second

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,14 +2,14 @@
 """The coroutine will loop 10 times, each time asynchronously wait 1 second"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """function that will loop the coroutine 10 times """
     for i in range(10):
         """Asynchronously wait for 1 second"""
 
         await asyncio.sleep(1)
         """The coroutine will loop 10 times"""
-
         yield random.uniform(0, 10)
