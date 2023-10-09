96d5683a2fc5baf8f00b139f1f6da2e230264c93
koussay557 <henidkousay@gmail.com>
2023-10-09 13:23:33 +0100

    fucntion that collect 10 random numbers using an async comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -5,10 +5,7 @@ from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension() -> List[float]:
     """Collect 10 random numbers using an async comprehension."""
     return [i async for i in async_generator()]
-
-async def main():
-    random_numbers = await async_comprehension()
-    print(random_numbers)
