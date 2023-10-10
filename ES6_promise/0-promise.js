b0dde77f8e9332e270605670fa2659dff582bb83
koussay557 <henidkousay@gmail.com>
2023-10-10 10:26:29 +0100

    function that return promis

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,10 +1,4 @@
 function getResponseFromAPI(){
-promise.then (function(result)
-{
-    console.log(result);
-})
-promise.catch(function (error)
-{
-    console.log(error);
-})
+    return Promise.resolve();
 }
+export default getResponseFromAPI;
