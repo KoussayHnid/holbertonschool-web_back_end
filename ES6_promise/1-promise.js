b39dc462536c725be7340fbfb56d43e3e86c7521
koussay557 <henidkousay@gmail.com>
2023-10-10 11:56:15 +0100

    function that return a promise with boolean parameter

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,9 +1,10 @@
 function getFullResponseFromAPI(success) {
-    return Promise.resolve();
     if (success) {
         resolve({ status: 200, body:'Success' });
     }
     else
-    reject(Error('The fake API is not working currently'))
+        reject(Error('The fake API is not working currently'))
+    return new Promise(resolve, reject)
+   
 }
 export default getFullResponseFromAPI;
