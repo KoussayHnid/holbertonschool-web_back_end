bc4b6341faa7f2068dd3d7b9f474a4930573109c
koussay557 <henidkousay@gmail.com>
2024-02-15 13:33:00 +0100

    Add LIFOCache class implementation

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-""" LIFOCache module
-"""
-from base_caching import BaseCaching
+""" LIFOCache module"""
 
+BaseCaching = __import__('base_caching').BaseCaching
 
 class LIFOCache(BaseCaching):
-    """ LIFOCache inherits from BaseCaching and is a caching system.
-    """
+    """ LIFOCache inherits from BaseCaching and is a caching system """
 
     def __init__(self):
-        """ Initialize LIFOCache instance.
-        """
+        """ Initialize LIFOCache instance"""
         super().__init__()
 
     def put(self, key, item):
-        """ Add an item to the cache using LIFO algorithm.
-        """
+        """ Add an item to the cache using LIFO algorithm"""
         if key is None or item is None:
             return
 
         if len(self.cache_data) >= self.MAX_ITEMS:
-            # Remove the last item inserted (LIFO)
             discarded_key = next(reversed(self.cache_data))
             del self.cache_data[discarded_key]
             print("DISCARD:", discarded_key)
@@ -28,13 +23,11 @@ class LIFOCache(BaseCaching):
         self.cache_data[key] = item
 
     def get(self, key):
-        """ Retrieve an item from the cache.
-        """
+        """ Retrieve an item from the cache"""
         if key is None or key not in self.cache_data:
             return None
-
+        
         return self.cache_data[key]
-
 
 if __name__ == "__main__":
     my_cache = LIFOCache()
