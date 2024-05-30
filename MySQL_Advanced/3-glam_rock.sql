643b3689e6265e6f690c7af0ee553d35e5eb7d77
koussay557 <henidkousay@gmail.com>
2024-05-30 14:20:33 +0100

    Add SQL script to list Glam rock bands ranked by their longevity

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,6 @@
 -- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+-- You should use attributes formed and split for computing the lifespan
+-- Your script can be executed on any database
 
 SELECT band_name
 FROM metal_bands
