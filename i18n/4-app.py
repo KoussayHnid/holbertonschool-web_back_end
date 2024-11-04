02c65b763ec2d54a5151a5e21cb7d6af65c536e6
koussay557 <henidkousay@gmail.com>
2024-11-04 11:15:23 +0100

    Add support for forced locale selection via URL parameter

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,56 +1,29 @@
 #!/usr/bin/env python3
 """basic Flask app"""
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request
 from flask_babel import Babel
-import locale
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
+
+SUPPORTED_LANGUAGES = ['en', 'fr']
+
 babel = Babel(app)
 
 
-class Config:
-    """class cofig for the Flask app"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-app.config.from_object(Config)
-
-
-@babel.localeselector
 def get_locale():
-    """
-    detect if the incoming request contains locale argument
-    """
-    user = getattr(g, 'user', None)
-    if user is not None:
-        return user.locale
-
-    if request.args.get('locale') in app.config['LANGUAGES']:
-        return request.args.get('locale')
-
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    locale = request.args.get('locale')
+    if locale in SUPPORTED_LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(SUPPORTED_LANGUAGES)
 
 
-@app.before_request
-def before_request():
-    """
-    Set the user locale
-    """
-    if request.args.get('locale') in app.config['LANGUAGES']:
-        g.user = type(
-            'User', (object,), {
-                'locale': request.args.get('locale')})
+babel.init_app(app, locale_selector=get_locale)
 
 
-@app.route('/', methods=['GET'])
-def index():
-    """
-    Index template
-    """
+@app.route('/')
+def home():
     return render_template('4-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
