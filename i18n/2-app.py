9775a951ef13ae81d185be95faa88af094e4342a
koussay557 <henidkousay@gmail.com>
2024-11-04 11:00:27 +0100

    Add Flask app with Babel localization and custom get_locale function

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,37 +1,22 @@
 #!/usr/bin/env python3
 """basic Flask app"""
-from flask import Flask, render_template, request
+from flask import Flask, request
 from flask_babel import Babel
 
-app = Flask(__name__, template_folder='templates')
-babel = Babel(app)
+app = Flask(__name__)
 
+SUPPORTED_LANGUAGES = ['en', 'es', 'fr', 'de']
 
-class Config:
-    """class cofig for the Flask app"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+babel = Babel()
 
+def get_locale():
+    return request.accept_languages.best_match(SUPPORTED_LANGUAGES)
 
-app.config.from_object(Config)
-
+babel.init_app(app, locale_selector=get_locale)
 
 @app.route('/')
-def index():
-    """
-    index template
-    """
-    return render_template('2-index.html')
-
-
-@babel.localeselector
-def get_locale():
-    """
-    get_locale function
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
+def home():
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
