367b5ff411dffede2d15f011799654a9fdb3a6db
koussay557 <henidkousay@gmail.com>
2024-11-04 11:13:07 +0100

    Add Flask-Babel localization with English and French translations

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,45 +1,22 @@
 #!/usr/bin/env python3
 """basic Flask app"""
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
+
+SUPPORTED_LANGUAGES = ['en', 'fr']
+
 babel = Babel(app)
 
+def get_locale():
+    return request.accept_languages.best_match(SUPPORTED_LANGUAGES)
 
-class Config:
-    """class cofig for the Flask app"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-app.config.from_object(Config)
-
+babel.init_app(app, locale_selector=get_locale)
 
 @app.route('/')
-def index():
-    """
-    index template
-    """
-    return render_template('2-index.html')
-
-
-@babel.localeselector
-def get_locale():
-    """
-    get_locale function
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@app.route('/', methods=["GET"])
-def index():
-    """
-    index template
-    """
+def home():
     return render_template('3-index.html')
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
